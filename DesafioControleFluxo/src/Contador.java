cc954f38fdac6127a15692f6a77d834d22ecbb89
almdgustadev <gunna2513@gmail.com>
2024-10-01 18:58:45 -0300

    agora o codigo com exceçao

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -8,27 +8,35 @@ public class Contador {
 
         int parametroUm, parametroDois, interacao;
 
-        System.out.println("DIGITE O PRIMEIRO NUMERO: ");
-        parametroUm = input.nextInt();
-
-        System.out.println("DIGITE O SEGUNDO NUMERO: ");
-        parametroDois = input.nextInt();
-
-        if(parametroDois > parametroUm ){
+        try{
+            System.out.println("DIGITE O PRIMEIRO NUMERO: ");
+            parametroUm = input.nextInt();
+    
+            System.out.println("DIGITE O SEGUNDO NUMERO: ");
+            parametroDois = input.nextInt();
+    
+            if(parametroDois <= parametroUm ){
+                throw new NumeroInvalidoException("O SEGUNDO NÚMERO DEVE SER MAIOR QUE O PRIMEIRO");       
+            }
+            
             interacao = parametroDois - parametroUm;
 
-            for(int i = 1 ; i <= interacao ; i++ ){
+            for (int i = 1; i <= interacao; i++) {
                 System.out.println("Imprimindo o numero " + i);
-    
             }
     
+            
+        }catch (NumeroInvalidoException e) {
+            System.out.println(e.getMessage());
+        } finally{
             input.close();
-        }else{
-            System.out.println("O SEGUNDO NÚMERO DEVE SER MAIOR QUE O PRIMEIRO");
+
         }
         
-
         
+    }  
 
-    }
-}
+   
+  }
+
+
